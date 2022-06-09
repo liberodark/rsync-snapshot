03ce51f61b0a5702a2ced714785a98110a9f2f6d
liberodark <liberodark@gmail.com>
2022-06-09 22:35:06 +0200

    Add files via upload

---
+++ zfs_snapshot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# About: Rsync Snapshots
+# Author: liberodark
+# Thanks : erdnaxeli
+# License: GNU GPLv3
+
+version="0.1.0"
+
+echo "Welcome on Rsync Snapshots Script $version"
+
+#=================================================
+# CHECK ROOT
+#=================================================
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
+#=================================================
+# RETRIEVE ARGUMENTS FROM THE MANIFEST AND VAR
+#=================================================
+
+date=$(date +%Y.%m.%d_%H-%M-%S)
+dest="local-share"
+snapshots="24"
+lock="/tmp/zfs-snapshot.lock"
+remove=$(zfs list -t snapshot | head -n -"${snapshots}" | grep "${dest}" | awk '{print $1}')
+
+# Remove Old Snapshot
+zfs destroy "${remove}"
+
+exec 9>"${lock}"
+flock -n 9 || exit
+
+# Backup
+zfs snapshot "${dest}"@"${date}"
